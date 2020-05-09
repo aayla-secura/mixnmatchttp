aayla-secura/mixnmatchttp

commit 9a4b3df236dcf53b664e868f562ed3b33a80692a

Bug fix
Fixes a "bug fix" in 05f96c033b37875a1187bdf780dbf3ffccb98968. Instead
of checking of the header value is an Iterable (which would treat
strings as lists), we should be checking for non-string or non-dict
iterables.

mixnmatchttp/utils.py
@@ -76,6 +76,9 @@ else:
 
 __all__ = [
     'DictNoClobber',
+    'is_str_like',
+    'is_seq_like',
+    'is_map_like',
     'abspath',
     'iter_abspath_up_to_nth',
     'iter_abspath',
@@ -100,7 +103,7 @@ class DictNoClobber(UserDict, object):
                 'expected at most 1 arguments, got %d' % len(args))
         if args:
             other = args[0]
-            if isinstance(other, _abcoll.Mapping):
+            if is_map_like(other):
                 for key in other:
                     callback(key, other[key])
             elif hasattr(other, 'keys'):
@@ -167,6 +170,36 @@ class DictNoClobber(UserDict, object):
         except AttributeError:
             # python3
             return super().items()
+
+
+def is_str_like(val):
+    '''True if val can be converted to string straightforwardly
+
+    i.e. if it's a string, boolean or int
+    '''
+
+    num_types = (int, float, bool)
+    try:  # python2
+        str_types = (basestring,)
+    except NameError:  # python3
+        str_types = (str, bytes)
+    return isinstance(val, num_types + str_types)
+
+def is_seq_like(val):
+    '''True if val is an iterable (but not a string, or dict)
+
+    i.e. if it's a list, set, tuple, or some mutable sequence
+    '''
+
+    return isinstance(val, _abcoll.Iterable) \
+        and not is_str_like(val) \
+        and not is_map_like(val)
+
+def is_map_like(val):
+    '''True if val is like a dict (mapping)'''
+
+    # in python2 UserDict is not a child of Mapping
+    return isinstance(val, (_abcoll.Mapping, UserDict))
 
 def abspath(path):
     '''Canonicalize the path segment by segment

mixnmatchttp/handlers/authenticator.py
@@ -15,12 +15,6 @@ import re
 from random import randint
 from datetime import datetime
 from collections import OrderedDict
-try:
-    # python2
-    from collections import _abcoll
-except ImportError:
-    # python3
-    from collections import abc as _abcoll
 import hashlib
 try:
     from passlib import hash as unix_hash
@@ -28,7 +22,7 @@ except ImportError:
     pass  # it's an optional feature
 
 from .. import endpoints
-from ..utils import param_dict, \
+from ..utils import is_seq_like, is_map_like, param_dict, \
     datetime_to_timestamp, date_from_timestamp, \
     curr_timestamp, UTCTimeZone
 from .base import BaseMeta, BaseHTTPRequestHandler
@@ -119,6 +113,12 @@ class BaseAuthHTTPRequestHandlerMeta(BaseMeta):
         def isoneof(val, sequence):
             return val in sequence
 
+        def isanytrue(val, checkers):
+            for c in checkers:
+                if c(val):
+                    return True
+            return False
+
         new_class = super().__new__(cls, name, bases, attrs)
         pwd_types = [None]
         prefT = '_transform_password_'
@@ -136,8 +136,7 @@ class BaseAuthHTTPRequestHandlerMeta(BaseMeta):
         requirements = {
             '_pwd_type': (isoneof, pwd_types),
             '_SameSite': (isoneof, [None, 'lax', 'strict']),
-            '_secrets': (isinstance,
-                         (_abcoll.Iterable, _abcoll.Mapping)),
+            '_secrets': (isanytrue, [is_seq_like, is_map_like]),
             '_pwd_min_len': (isinstance, int),
             '_pwd_min_charsets': (isinstance, int),
             '_jwt_lifetime': (isinstance, int),

mixnmatchttp/handlers/base.py
@@ -14,12 +14,6 @@ import shutil
 import mimetypes
 import urllib
 import json
-try:
-    # python2
-    from collections import _abcoll
-except ImportError:
-    # python3
-    from collections import abc as _abcoll
 try:  # python3
     from json import JSONDecodeError
 except ImportError:  # python2
@@ -31,7 +25,7 @@ from string import Template
 from future.utils import with_metaclass
 
 from .. import endpoints
-from ..utils import abspath, param_dict, DictNoClobber
+from ..utils import is_seq_like, abspath, param_dict, DictNoClobber
 
 __all__ = [
     'methodhandler',
@@ -402,7 +396,7 @@ class BaseHTTPRequestHandler(with_metaclass(
         '''
 
         for h, v in headers.items():
-            if isinstance(v, _abcoll.Iterable):
+            if is_seq_like(v):
                 for i in v:
                     self.send_header(h, i)
             else:

commit 36fb3e6268a7851533223276f6b5c7a2ebecff98

Another bug fix
A change in DictNoCLobber in 9a4b3df236dcf53b664e868f562ed3b33a80692a
broke things for python2, fixed.

mixnmatchttp/utils.py
@@ -103,7 +103,7 @@ class DictNoClobber(UserDict, object):
                 'expected at most 1 arguments, got %d' % len(args))
         if args:
             other = args[0]
-            if is_map_like(other):
+            if isinstance(other, _abcoll.Mapping):
                 for key in other:
                     callback(key, other[key])
             elif hasattr(other, 'keys'):