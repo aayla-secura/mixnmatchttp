f84c839dd62a977cd44c2aebb7947cb3ed109ac7
Aayla Secura <aayla.secura.1138@gmail.com>
2020-05-22 17:29:29 +1200

    Changed version in setup.py: beta -> dev1
    Not ready for beta yet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='mixnmatchttp',
-    version='1.0beta',
+    version='1.0.dev1',
     url='https://github.com/aayla-secura/mixnmatchttp',
     author='AaylaSecura1138',
     author_email='aayla.secura.1138@gmail.com',
