07023a5cfd28f5079dba233b41111d2d9c773ace
Aayla Secura <aayla.secura.1138@gmail.com>
2020-06-24 07:42:25 +1200

    v1.0.dev24 -> 1.0.dev25

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='mixnmatchttp',
-    version='1.0.dev24',
+    version='1.0.dev25',
     url='https://github.com/aayla-secura/mixnmatchttp',
     author='AaylaSecura1138',
     author_email='aayla.secura.1138@gmail.com',
