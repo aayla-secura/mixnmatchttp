a3d8e3413cad8cb998ec1e545d8563ce0288d796
Aayla Secura <aayla.secura.1138@gmail.com>
2021-07-08 10:43:52 +1200

    x

--- mixnmatchttp/utils/io.py
+++ mixnmatchttp/utils/io.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 __all__ = [
     'open_path',
     'read_file',
+    'is_modified_since',
 ]
 
 
@@ -33,3 +34,13 @@ def read_file(path, mode='r'):
     with open_path(path, mode=mode) as (fd, filename):
         logger.debug('Reading file {}'.format(filename))
         return fd.read()
+
+def is_modified_since(path, last_ts):
+    '''True if path's been modified since last_ts timestamp'''
+
+    if hasattr(path, 'read'):
+        fs = os.fstat(path)
+    else:
+        fs = os.stat(path)
+
+    return fs.st_mtime > last_ts
